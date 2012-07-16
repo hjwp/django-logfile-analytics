0147735c8f1d8699b0eb1922c09a95b857dfb7df
Harry <hjwp2@cantab.net>
2012-07-16 11:50:54 +0100

    first cut of parser. almost works. by: HP

--- analytics/parser.py
+++ analytics/parser.py
@@ -1,3 +1,31 @@
+import csv
+from datetime import datetime
+from analytics.models import Hit
+
+def quote_fixing_iterator(filename):
+    with open(filename) as f:
+        for row in f.readlines():
+            yield row.replace('[', '"').replace(']', '"')
+
 
 def parse_logfile(filename):
-    pass
+    row_parser = csv.reader(quote_fixing_iterator(filename), delimiter=" ")
+    for row in row_parser:
+        print row
+        source_ip, _, __, timestamp_str, method_url_protocol, status_code, response_size, referer_url, user_agent = row
+        try:
+            response_size = int(response_size)
+        except ValueError:
+            response_size = 0
+        method, url, _ = method_url_protocol.split()
+        timestamp = datetime.strptime(timestamp_str[:-6], '%d/%b/%Y:%H:%M:%S')
+        Hit(
+                source_ip=source_ip,
+                timestamp=timestamp,
+                method=method,
+                url=url,
+                referer_url=referer_url,
+                status_code=status_code,
+                response_size=response_size,
+                user_agent=user_agent
+        ).save()
--- analytics/tests/tests.py
+++ analytics/tests/tests.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 from django.test import TestCase
 
 from analytics.models import Hit
@@ -20,14 +21,14 @@ class HitModelTest(TestCase):
 class ParserTest(TestCase):
 
     def test_parsing_a_logfile(self):
-        filename = 'test_log_data_small.log'
+        filename = os.path.join(os.path.dirname(__file__), 'test_log_data_small.log')
         parse_logfile(filename)
         self.assertEqual(Hit.objects.all().count(), 3)
-        self.assertEqual(Hit.objects.all(), Hit.objects.all().order_by('timestamp'))
+        self.assertItemsEqual(Hit.objects.all(), Hit.objects.all().order_by('timestamp'))
 
         hit1 = Hit.objects.all()[0]
-        self.assertEqual(hit1.source_ip, '78.86.125.186')
-        self.assertEqual(hit1.timestamp, datetime('2011-07-21 17:49:33'))
+        self.assertEqual(hit1.source_ip, '77.86.125.186')
+        self.assertEqual(hit1.timestamp, datetime(year=2011,month=9,day=21, hour=17, minute=49,second=33))
         self.assertEqual(hit1.method, 'GET')
         self.assertEqual(hit1.url, '/')
         self.assertEqual(hit1.status_code, 500)
@@ -38,27 +39,23 @@ class ParserTest(TestCase):
 
         hit2 = Hit.objects.all()[1]
         self.assertEqual(hit2.source_ip, '78.86.125.186')
-        self.assertEqual(hit2.timestamp, datetime('2011-07-21 18:11:19'))
+        self.assertEqual(hit2.timestamp, datetime(year=2011,month=9,day=21, hour=18, minute=11,second=19))
         self.assertEqual(hit2.method, 'POST')
         self.assertEqual(hit2.url, '/accounts/login/')
         self.assertEqual(hit2.status_code, 200)
         self.assertEqual(hit2.response_size, 744)
-        self.assertEqual(hit2.referer_url, 'http://www.pythonanywhere.com/')
+        self.assertEqual(hit2.referer_url, 'http://harry.pythonanywhere.com/')
         self.assertEqual(hit2.user_agent, 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.186 Safari/535.1')
 
 
         hit3 = Hit.objects.all()[2]
         self.assertEqual(hit3.source_ip, '66.249.71.146')
-        self.assertEqual(hit3.timestamp, datetime('2012-05-07 17:49:33'))
+        self.assertEqual(hit3.timestamp, datetime(year=2012,month=5,day=7, hour=12, minute=42,second=20))
         self.assertEqual(hit3.method, 'GET')
-        self.assertEqual(hit3.url, 'static/images/admin03t.png')
+        self.assertEqual(hit3.url, '/static/images/admin03t.png')
         self.assertEqual(hit3.status_code, 304)
-        self.assertEqual(hit3.response_size, None)
+        self.assertEqual(hit3.response_size, 0)
         self.assertEqual(hit3.referer_url, None)
         self.assertEqual(hit3.user_agent, 'Googlebot-Image/1.0')
 
 
-
-    def test_parsing_single_line(self):
-        line = '78.86.125.186 - - [21/Sep/2011:17:26:56 +0000] "GET / HTTP/1.1" 500 404 "http://www.pythonanywhere.com/user/harry/webapps/" "Mozilla/5.0 (Windows NT 6.0; rv:6.0.2) Gecko/20100101 Firefox/6.0.2"'
-        self.fail('todo')
